9cc2b8a660389336282c5f30c17fbd10c9f87205
Dasha <dashakonopelko@gmail.com>
2025-05-07 16:53:53 +0300

    HttpProtocol

---
+++ 08_Http_protocol_GET_POST_Methods/Http_protocol_GET_POST_Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Post
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int UserId { get; set; }
+}
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Get
+
+        /*var url = "https://jsonplaceholder.typicode.com/users";
+        using var client = new HttpClient();
+
+        var response = await client.GetAsync(url);
+
+        Console.WriteLine($"Status : {response.StatusCode}");
+        var result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(result);*/
+
+        // Post
+        Post post = new()
+        {
+            Title = "TestTitle",
+            Body = "TestBody",
+            UserId = 1
+        };
+
+        var json = JsonSerializer.Serialize(post);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var url = "https://jsonplaceholder.typicode.com/posts";
+
+        using var client = new HttpClient();
+        var response = await client.PostAsync(url, data);
+
+        Console.WriteLine($"Status : {response.StatusCode}");
+        var result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(result);
+
+
+    }
+}
